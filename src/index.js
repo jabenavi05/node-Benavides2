d46ef2c28a348fa56fc8cb47f237328277cc6627
jabenavi05 <jorgebenavides05@hotmail.com>
2021-09-05 23:40:43 -0500

    coreccion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import app from "./app";
+import './database';
 
-app.listen(3000);
-console.log('Server listen on port', 3000)
+app.listen(app.get("port"));
+
+console.log("Server on port", app.get('port'));
